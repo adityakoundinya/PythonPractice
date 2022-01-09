3423c032b4456bf4cbf9f14beb9726cefff07c07
Adi Koundinya <akoundinya@wayfair.com>
2022-01-09 00:05:51 -0600

    Adding play till exit

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,17 +1,22 @@
 import random
 
-choices = ["rock", "paper", "scissors"];
-computerChoice = random.choice(choices);
-userChoice = input("Whats your pick? \n");
+isExit = 1;
 
-if(computerChoice == userChoice):
-  print("Tie");
-elif userChoice == 'paper' and computerChoice == 'rock':
-  print("win");
-elif userChoice == 'scissors' and computerChoice == 'paper':
-  print("win");
-elif userChoice == 'rock' and computerChoice == 'scissors':
-  print("win");
-else:
-  print("Lose");
-print("You chose " + userChoice + " computer chose " + computerChoice);
+while(isExit == 1):
+  choices = ["rock", "paper", "scissors"];
+  computerChoice = random.choice(choices);
+  userChoice = input("Whats your pick? \n");
+  if userChoice == 'exit':
+    isExit = 0;
+    break;
+  if(computerChoice == userChoice):
+    print("Tie");
+  elif userChoice == 'paper' and computerChoice == 'rock':
+    print("win");
+  elif userChoice == 'scissors' and computerChoice == 'paper':
+    print("win");
+  elif userChoice == 'rock' and computerChoice == 'scissors':
+    print("win");
+  else:
+    print("Lose");
+  print("You chose " + userChoice + " computer chose " + computerChoice);

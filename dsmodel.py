a21ad3b123be3cac5a636a4ebfe4c8e0bd88878a
Adi Koundinya <akoundinya@wayfair.com>
2023-01-11 14:53:44 -0600

    Adding Dsmodel client

--- dsmodel.py
+++ dsmodel.py
@@ -1,13 +1,17 @@
 from wayml.python_client.wayml_client_async import WayMLClientAsync
 import asyncio
+from os import environ
 
+environ["GRPC_DNS_RESOLVER"] = "native"
 handler_name = "CustomerScoreUnifiedModelV2Handler"
-k8s_ingress = "kube-sweetwater-customer-score-unified-model--v2-grpc.service.intradsm1.sdeconsul.csnzoo.com"
+k8s_ingress_dev = "kube-sweetwater-customer-score-unified-model--v2-grpc.service.intradsm1.sdeconsul.csnzoo.com"
+k8s_ingress_prod = "kube-sweetwater-customer-score-unified-model--v2-grpc.service.intraiad1.consul.csnzoo.com"
 
 
 async def call_server_async():
     # client = await WayMLClientAsync.get_instance(handler_name, "localhost:50051")
-    client = await WayMLClientAsync.get_instance(handler_name, k8s_ingress)  # dev/prod client
+    # client = await WayMLClientAsync.get_instance(handler_name, k8s_ingress_dev)  # dev client
+    client = await WayMLClientAsync.get_instance(handler_name, k8s_ingress_prod)  # prod client
     model_input = {"customer_guid": "cccccccc-ffff-eeee-fecd-000000000101",
                    "device_guids": [
                        "23e17d3a-60d1-546a-820c-11a9c61ec702",
